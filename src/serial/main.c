23ebfe13752f385fd30a861cb7c3d7dea24bc1f3
Steven Fernández <vsteven25@gmail.com>
2023-06-04 18:14:22 -0600

    Add GetLU function

--- src/serial/main.c
+++ src/serial/main.c
@@ -147,6 +147,140 @@ double* getD(double* U)
     return D;
 }
 
+void add_column(double* src_col, double* dst_mtx, size_t dst_col) {
+    for(size_t row = 0; row < 8; ++row) {
+        // printf("dst (%f) += %f\n", dst_mtx[row * nNodos + dst_col], src_col[row]);
+        dst_mtx[row * nNodos + dst_col] += src_col[row];
+    }
+    // printf("End\n\n");
+}
+
+double* multiply_column(double value, size_t col, double *mtx) {
+    double *a_result = malloc(8 * sizeof(double));
+
+    for(size_t row = 0; row < 8; ++row) {
+        a_result[row] = value * mtx[row * nNodos + col];
+    }
+
+   return a_result;
+}
+
+double* getLU(double* U, double *x) {
+    double* rho = U + (3 * nNodos);
+    double length = nNodos;
+    double etaV = 0.001;
+    double etaT = 0.001;
+    double a1 = 1.0 / 60;
+    double a2 = -3.0 / 20;
+    double a3 = 3.0 / 4;
+    double a4 = -3.0 / 4;
+    double a5 = 3.0 / 20;
+    double a6 = -1.0 / 60;
+    
+    double b1 = 1.0 / 90;
+    double b2 = -3.0 / 20;
+    double b3 = 3.0 / 2;
+    double b4 = -49.0 / 18;
+    double b5 = 3.0 / 2;
+    double b6 = -3.0 / 20;
+    double b7 = 1.0 / 90;
+    
+    // Allocate memory for arrays
+    double* F = getF(U);
+    double* D = getD(U);
+    double* dummy1 = (double *)malloc(8 * length * sizeof(double));
+    for (size_t i = 0; i < 8 * length; i++)
+    {
+        dummy1[i] = 0;
+    }
+
+    double* dummy2 = (double *)malloc(8 * length * sizeof(double));
+    for (size_t i = 0; i < 8 * length; i++)
+    {
+        dummy2[i] = 0;
+    }
+
+    // Calculate dummy1 array
+    //parallel
+    for (size_t i = 3; i < length - 3; i++)
+    {
+        double* a1_val = multiply_column(a1, i+3, F);
+        double* a2_val = multiply_column(a2, i+2, F);
+        double* a3_val = multiply_column(a3, i+1, F);
+        double* a4_val = multiply_column(a4, i-1, F);
+        double* a5_val = multiply_column(a5, i-2, F);
+        double* a6_val = multiply_column(a6, i-3, F);
+        
+        double arreglo[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+        for (size_t arr_i = 0; arr_i < 8; arr_i++){
+            arreglo[arr_i] = a1_val[arr_i] + a2_val[arr_i] + a3_val[arr_i]+ a4_val[arr_i] + a5_val[arr_i] + a6_val[arr_i];
+        }
+
+        for(size_t row = 0; row < 8; ++row) {
+            dummy1[row * nNodos + i] = arreglo[row];
+        }
+
+        free(a1_val);
+        free(a2_val);
+        free(a3_val);
+        free(a4_val);
+        free(a5_val);
+        free(a6_val);
+    }
+
+    // Calculate dummy2 array
+    for (size_t i = 3; i < length - 3; i++)
+    {
+        double* b1_val = multiply_column(b1, i + 3, D);
+        double* b2_val = multiply_column(b2, i + 2, D);
+        double* b3_val = multiply_column(b3, i + 1, D);
+        double* b4_val = multiply_column(b4, i, D);
+        double* b5_val = multiply_column(b5, i - 1, D);
+        double* b6_val = multiply_column(b6, i - 2, D);
+        double* b7_val = multiply_column(b7, i - 3, D);
+        
+        double arreglo[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+        for (size_t arr_i = 0; arr_i < 8; arr_i++){
+            arreglo[arr_i] = b1_val[arr_i] + b2_val[arr_i] + b3_val[arr_i]+ b4_val[arr_i] + b5_val[arr_i] + b6_val[arr_i] + b7_val[arr_i];
+        }   
+
+        for(size_t row = 0; row < 8; ++row) {
+            dummy2[row * nNodos + i] = arreglo[row];
+        }
+    }
+
+    // Apply additional calculations to dummy2 array
+    for (size_t i = 0; i < length; i++)
+    {
+        dummy2[nNodos * 4 + i] = etaV * rho[i] * dummy2[nNodos * 4 + i];
+        dummy2[nNodos * 7 + i] = etaT * rho[i] * dummy2[nNodos * 7 + i];
+    }
+
+
+    double* LU = malloc(U_SIZE * sizeof(double));
+    for (size_t i = 0; i < U_SIZE; i++)
+    {
+        LU[i] = 0;
+    }
+
+    double dx = fabs(x[1] - x[0]);
+    // Calculate LU array
+    for (size_t i = 0; i < U_SIZE; i++)
+    {
+        LU[i] = (dummy1[i] / dx) + (dummy2[i] / pow2(dx));
+        // printf("LU[%ld] = %.10e\n", i, LU[i]);
+    }
+
+    // Free allocated memory
+    free(F);
+    free(D);
+    free(dummy1);
+    free(dummy2);
+
+    return LU;
+}
+ 
 void calculateAllValues(
     int length,
     double* x,     
@@ -193,7 +327,6 @@ void calculateAllValues(
 
 int main() {
     double* x = linspace(-1.0, 1.0, nNodos);
-    double dx = fabs(x[1] - x[0]);
 
     double* rho = malloc(nNodos * sizeof(double));
     double* Vx = malloc(nNodos * sizeof(double));
@@ -218,15 +351,14 @@ int main() {
     );
 
     double* U = getU(Bx, By, Bz, rho, p, Vx, Vy, Vz);
-
-    double* F = getF(U);
-    double* D = getD(U);
+    double* LU = getLU(U, x);
 
     for (size_t i = 0; i < U_SIZE; i++)
     {
         // printf("U[%ld] = %f\n", i, U[i]);
         // printf("F[%ld] = %f\n", i, F[i]);
-        printf("D[%ld] = %f\n", i, D[i]);
+        // printf("D[%ld] = %f\n", i, D[i]);
+        
     }
     
 
@@ -252,8 +384,7 @@ int main() {
     free(By);
     free(Bz);
     free(U);
-    free(F);
-    free(D);
+    free(LU);
 
     return 0;
 }
